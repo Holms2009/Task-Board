e31a3ff8bb41fe4789dedb54924862f7c41dcda4
Holms2009 <holms200990@gmail.com>
2021-09-18 23:29:37 +0300

    Login v0.1

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,10 +1,11 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, setDoc, updateDoc, doc,  } from 'firebase/firestore/lite';
 
-const loginButton = document.querySelector('.login-button');
-const userAvatar = document.querySelector('.user');
-
-const config = {
+const loginForm = document.querySelector('.page-header__login-form');
+const loginButton = loginForm.querySelector('.login-button');
+const userBlock = document.querySelector('.page-header__user');
+const name = userBlock.querySelector('.page-header__name');
+const firebaseConfig = {
   apiKey: "AIzaSyCBehvUdJ1Sww5gcdv4yBRRpetngqVoFiY",
   authDomain: "board-3a7c6.firebaseapp.com",
   projectId: "board-3a7c6",
@@ -13,26 +14,64 @@ const config = {
   appId: "1:457479426554:web:f1b1ea86d33b6ff9f95443",
   measurementId: "G-5SZC6Y976H"
 };
+const app = initializeApp(firebaseConfig);
+const firestoreDatabase = getFirestore(app);
+let activeUser;
 
-const app = initializeApp(config);
-const db = getFirestore(app);
+async function login(event) {
+  event.preventDefault();
+  const userName = loginForm.querySelector('.user-name');
+  const userPass = loginForm.querySelector('.user-password');
+  const users = await getData(firestoreDatabase);
+  const user = findUser(users, userName.value);
 
+  if (user instanceof Error) {
+    alert('No such user');
+    return
+  }
 
-async function getData() {
+  const allowLogin = checkPassword(user, userPass.value);
+
+  if (allowLogin) {
+    activeUser = doc(firestoreDatabase, `users`, userName.value);
+    commitLogin(activeUser);
+
+    userBlock.style.display = "inline-block";
+    name.innerHTML = userName.value;
+    loginForm.style.display = 'none';
+  } else {
+    alert('Wrong password');
+  }
+}
+
+async function getData(db) {
   const users = collection(db, 'users');
-  const snap = await getDocs(users);
-  const list = snap.docs.map(doc => doc.data());
+  const list = await getDocs(users).then(snap => snap.docs.map(doc => doc.data()));
+  return list
+}
 
-  console.log(list)
+function findUser(users, name) {
+  for (let i of users) {
+    if (i.name === name) {
+      return i;
+    }
+  }
+
+  return new Error('Wrong user name');
+}
+
+function checkPassword(user, pass) {
+  return user.password === pass ? true : false;
+}
+
+async function commitLogin(user) {
+  try {
+    const request = await updateDoc(user, {
+        login: true
+    })
+  } catch(e) {
+    console.error("Error:", e);
+  }
 }
 
 loginButton.addEventListener('click', login);
-
-function login() {
-  const userName = document.querySelector('.user-name');
-  const userPass = document.querySelector('.user-password');
-  getData();
-
-  userAvatar.style.display = "inline-block";
-  userAvatar.innerHTML = `Пользователь ${userName.value} вошел`;
-}
